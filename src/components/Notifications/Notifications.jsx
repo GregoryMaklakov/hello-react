fe4efb10a8dce4daa8527e787709185d066a2a60
Gregory <maklakov.grigory@gmail.com>
2023-02-22 11:08:45 +0100

    fix svg bug

--- src/components/Notifications/Notifications.jsx
+++ src/components/Notifications/Notifications.jsx
@@ -1,11 +1,11 @@
 import clsx from "clsx";
 import PropTypes from "prop-types"; // ES6
 
-import {IconSuccess, IconWarning, IconError} from '../icons';
+// import {IconSuccess, IconWarning, IconError} from '../icons';
 
-// import IconSuccess from "../../assets/svg/success.svg";
-// import IconWarning from "../../assets/svg/warning.svg";
-// import IconError from "../../assets/svg/error.svg";
+import IconSuccess from "../../assets/svg/success.svg";
+import IconWarning from "../../assets/svg/warning.svg";
+import IconError from "../../assets/svg/error.svg";
 import styles from "./Notifications.module.css";
 
 const icons = {
@@ -14,7 +14,7 @@ const icons = {
   error: IconError,
 };
 
-const Icon = icons[type];  
+// const Icon = icons[type];  
 
 export const Notification = ({ className, type, children, Icon }) => {
   const onClick = () => {
@@ -26,9 +26,9 @@ export const Notification = ({ className, type, children, Icon }) => {
       onClick={onClick}
       className={clsx(styles.button, styles[type], className)}
     >
-        <Icon className={styles.icon}></Icon>
+        {/* <Icon className={styles.icon}></Icon> */}
 
-      {/* <img className={styles.icon} src={icons[type]} alt={type} /> */}
+      <img className={styles.icon} src={icons[type]} alt={type} />
       <span className={styles.label}>{children}</span>
     </button>
   );
